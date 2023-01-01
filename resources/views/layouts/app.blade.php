907787e4a1d3fdbf0b70ce1dce527e1c619e7ee6
Jorge Alfaro <jorgm.alfarc@gmail.com>
2023-01-01 15:20:39 -0600

    blade img

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,7 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-sm">
             <div class="container">
-{{--                <img src="{{ Vite::asset('resources//img/identicon.svg') }}" class="me-1" alt="logo">--}}
+                <img src="{{ Vite::asset('resources//img/identicon.svg') }}" class="me-1" alt="logo">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
