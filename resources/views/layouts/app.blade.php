2dd52fc23cf84ea633613249630856d15ec6ed84
Jorge Alfaro <jorgealfaro@grupoicarus.com.mx>
2023-01-02 09:23:44 -0600

    commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,8 +14,8 @@
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
     <!-- Scripts -->
-{{--    @vite(['resources/sass/app.scss','resources/css/app.css', 'resources/js/app.js' ])--}}
-{{--    @stack('scripts')--}}
+    @vite(['resources/sass/app.scss','resources/css/app.css', 'resources/js/app.js' ])
+    @stack('scripts')
 </head>
 <body>
     <div id="app">
