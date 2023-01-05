1ebcc13bf499ea1a93cbca145b02e4b4a5403f47
Jorge Alfaro <jorgealfaro@grupoicarus.com.mx>
2023-01-05 10:14:53 -0600

    updated event index

--- resources/views/event/index.blade.php
+++ resources/views/event/index.blade.php
@@ -8,30 +8,37 @@
                 <div class="text-center">
                     <div class="btn-group" role="group" aria-label="Basic example">
                         <button type="button" class="btn btn-secondary rounded-start" data-bs-toggle="offcanvas"
-                                data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">Crear
+                                data-bs-target="#offcanvasEventCreate" aria-controls="offcanvasEventCreate">Crear
                         </button>
-                        <button type="button" class="btn btn-secondary me-1 ms-1">Edita</button>
-                        <button type="button" class="btn btn-secondary rounded-end">Elimina</button>
+                        <button type="button" class="btn btn-secondary me-1 ms-1" data-bs-toggle="offcanvas"
+                                data-bs-target="#offcanvasEventEdit" aria-controls="offcanvasEventEdit">Edita
+                        </button>
+                        <button type="button" class="btn btn-secondary rounded-end" data-bs-toggle="offcanvas"
+                                data-bs-target="#offcanvasEventDelete" aria-controls="offcanvasEventDelete">Elimina
+                        </button>
                     </div>
                 </div>
-
-                <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+                <!--Canvas create -->
+                <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasEventCreate"
+                     aria-labelledby="offcanvasExampleLabel">
                     <div class="offcanvas-header">
                         <h5 class="offcanvas-title" id="offcanvasExampleLabel">Evento</h5>
-                        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas"
+                                aria-label="Close"></button>
                     </div>
                     <div class="offcanvas-body">
                         <div class="container">
                             <div class="row justify-content-center">
                                 <div class="col-md-8">
                                     <div class="card">
-                                        <div class="card-header">Agrega un evento</div>
+                                        <div class="card-header"><i class="fa-brands fa-get-pocket"></i>  Agrega un evento</div>
 
                                         <div class="card-body">
                                             <form method="POST" action="{{ route('events.store') }}">
                                                 @csrf
                                                 <div class="row mb-3">
-                                                    <label for="name" class="col-md-4 col-form-label text-md-end">Nombre</label>
+                                                    <label for="name"
+                                                           class="col-md-4 col-form-label text-md-end">Nombre</label>
                                                     <div class="col-md-6">
                                                         <input id="name" type="text" class="form-control" name="name"
                                                                placeholder="Ejemplo fiesta de cumpleaños"
@@ -40,7 +47,7 @@
                                                 </div>
                                                 <div class="row mb-0">
                                                     <div class="col-md-6 offset-md-4">
-                                                        <button type="submit" class="btn btn-primary">
+                                                        <button type="submit" class="btn btn-success">
                                                             Guardar
                                                         </button>
                                                     </div>
@@ -54,12 +61,64 @@
                         </div>
                     </div>
                 </div>
-{{--                @if (session('status'))--}}
-{{--                    <div class="alert alert-dismissible alert-success">--}}
-{{--                        <button type="button" class="btn-close" data-bs-dismiss="alert"></button>--}}
-{{--                        <strong> {{ session('status') }}</strong>--}}
-{{--                    </div>--}}
-{{--                @endif--}}
+                <!--Canvas Edit -->
+                <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasEventEdit"
+                     aria-labelledby="offcanvasExampleLabel">
+                    <div class="offcanvas-header">
+                        <h5 class="offcanvas-title" id="offcanvasExampleLabel">Evento</h5>
+                        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas"
+                                aria-label="Close"></button>
+                    </div>
+                    <div class="offcanvas-body">
+                        <div class="container">
+                            <div class="row justify-content-center">
+                                <div class="col-md-8">
+                                    <div class="card">
+                                        <div class="card-header"><i class="fa-solid fa-wand-magic-sparkles"></i>  Edita un evento</div>
+                                        <div class="card-body">
+                                            <!--foreach TODO-->
+                                            <form method="POST" action="{{ route('events.create') }}">
+                                                @csrf
+                                                <div class="form-group">
+                                                    <div class="form-group">
+                                                        <div class="input-group mb-3">
+                                                            <input type="text" class="form-control" id="name"
+                                                                   name="name" value="" required
+                                                                   placeholder="Ejemplo fiesta de cumpleaños"
+                                                                   aria-label="Ejemplo fiesta de cumpleaños"
+                                                                   aria-describedby="button-addon2">
+                                                            <button class="btn btn-success" type="button"
+                                                                    id="button-addon2">Guardar
+                                                            </button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </form>
+                                            <!--endforeach -->
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--Canvas Delete -->
+                <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasEventDelete"
+                     aria-labelledby="offcanvasExampleLabel">
+                    <div class="offcanvas-header">
+                        <h5 class="offcanvas-title" id="offcanvasExampleLabel">Evento</h5>
+                        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas"
+                                aria-label="Close"></button>
+                    </div>
+                    <div class="offcanvas-body">
+                        <div>
+                             // Contenido
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Inicio -->
                 <div class="card mt-2">
                     <div class="card-header">Lista de eventos</div>
                     <div class="card-body">
@@ -74,25 +133,33 @@
                         </ul>
                     </div>
                 </div>
-                <form method="POST" action="{{ route('events.update') }}">
-                    @csrf
-                    @method('PUT')
-                    <div class="form-group">
-                        <label for="event_id" class="form-label mt-4">Cambia el estatus del evento..</label>
-                        <div class="d-flex">
-                            <select class="form-select" id="event_id" name="event_id">
-                                @if(count(Event::all()) == 0)
-                                    <option value="">No hay eventos creados</option>
-                                @endif
-                                @foreach(Event::all() as $eve)
-{{--                                    //Active the status TODO--}}
-                                    <option value="{{ $eve->id }}">{{$eve->name}} @if($event->status) OKO @endif</option>
-                                @endforeach
-                            </select>
-                            <button class="btn btn-outline-danger" type="submit">Cambiar</button>
-                        </div>
+                <div class="card border-danger mb-3 mt-3">
+                    <div class="card-header">Cambia el estatus del evento.</div>
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('events.change') }}">
+                            @csrf
+                            @method('PUT')
+                            <div class="form-group">
+                                <div class="d-flex">
+                                    <select class="form-select" id="event_id" name="event_id">
+                                        @if(count(Event::all()) == 0)
+                                            <option value="">No hay eventos creados</option>
+                                        @endif
+                                        @foreach(Event::all() as $eve)
+                                            @if($eve->status === true)
+                                                <option selected class="text-success" value="{{ $eve->id }}">{{$eve->name}} - Active</option>
+                                            @else
+                                                <option value="{{ $eve->id }}">{{$eve->name}} </option>
+                                            @endif
+                                        @endforeach
+                                    </select>
+                                    <button class="btn btn-outline-danger" type="submit">Cambiar</button>
+                                </div>
+                            </div>
+                        </form>
                     </div>
-                </form>
+                </div>
+
             </div>
         </div>
     </div>
