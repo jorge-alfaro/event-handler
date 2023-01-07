67f0ff379669fdfa94006cd9a831b183ae27fc11
Jorge Alfaro <jorgealfaro@grupoicarus.com.mx>
2023-01-07 09:03:00 -0600

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,74 +2,75 @@
 
 @section('content')
 
-    <div class="container">
-        <div class="row mt-2">
-            <div class="col">
-                <div class="list-group">
-                    <a href="#" class="list-group-item list-group-item-action active">Evento</a>
-                    @if($event)
-                        <a href="#" class="list-group-item list-group-item-action">{{ $event->name }}</a>
-                    @else
-                        <a href="#" class="list-group-item list-group-item-action"> No hay eventos activos</a>
-                    @endif
+    <div class="container-sm">
+        <div class="col-md-6 offset-md-3">
+            <div class="row mt-2">
+                <div class="col">
+                    <div class="list-group">
+                        <a href="#" class="list-group-item list-group-item-action active">Evento</a>
+                        @if($event)
+                            <a href="#" class="list-group-item list-group-item-action">{{ $event->name }}</a>
+                        @else
+                            <a href="#" class="list-group-item list-group-item-action"> No hay eventos activos</a>
+                        @endif
 
+                    </div>
                 </div>
             </div>
+            <div class="row mt-2">
+                <div class="col">
+                    <div class="card">
+                        <ul class="list-group">
+                            <a href="#" class="list-group-item list-group-item-action active bg-success">Cuenta</a>
 
-        </div>
-        <div class="row mt-2">
-            <div class="col">
-                <div class="card">
-                    <ul class="list-group">
-                        <a href="#" class="list-group-item list-group-item-action active bg-success">Cuenta</a>
-
-                        @foreach($products as $p)
+                            @foreach($products as $p)
+                                <li class="list-group-item d-flex justify-content-between align-items-center">
+                                    {{$p->name}}
+                                    <span class="badge bg-success rounded-pill"> $ {{ $p->price }}</span>
+                                </li>
+                            @endforeach
+                            <!-- TOTAL -->
                             <li class="list-group-item d-flex justify-content-between align-items-center">
-                                {{$p->name}}
-                                <span class="badge bg-success rounded-pill"> $ {{ $p->price }}</span>
+                                Total
+                                <button type="button" class="btn btn-outline-success">$ {{ $total }}</button>
                             </li>
-                        @endforeach
-                        <!-- TOTAL -->
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="mt-2">
+                    <ul class="list-group">
+                        <a href="#" class="list-group-item list-group-item-action active">Total por miembro</a>
                         <li class="list-group-item d-flex justify-content-between align-items-center">
-                            Total
-                            <button type="button" class="btn btn-outline-success">$ {{ $total }}</button>
+                            <i class="fa-solid fa-user"></i>
+                            <button type="button" class="btn btn-outline-success">$ {{ $theCheck }}</button>
                         </li>
                     </ul>
                 </div>
-            </div>
 
-            <div class="mt-2">
-                <ul class="list-group">
-                    <a href="#" class="list-group-item list-group-item-action active">Total por miembro</a>
-                    <li class="list-group-item d-flex justify-content-between align-items-center">
-                        <i class="fa-solid fa-user"></i>
-                        <button type="button" class="btn btn-outline-success">$ {{ $theCheck }}</button>
-                    </li>
-                </ul>
-            </div>
+                <div class="card mt-2">
+                    <ul class="list-group">
+                        <a href="#" class="list-group-item list-group-item-action active">Miembros</a>
+                        @if(!$members)
+                            <li class="list-group-item d-flex justify-content-between align-items-center">
+                                <i class="fa-solid fa-user-slash"></i> No hay miembros registrados.
+                            </li>
+                        @endif
+                        @foreach($members as $m)
+                            <li class="list-group-item d-flex justify-content-between align-items-center">
+                                {{$m->name}}
+                                @if($m->payment_status['paid'])
+                                    <span class="badge bg-success rounded-pill"> Pagado</span>
+                                @elseif($m->payment_status['a_piece'])
+                                    <span class="badge bg-success rounded-pill"> Abonado</span>
+                                @else
+                                    <span class="badge bg-success rounded-pill"> Pendiente</span>
+                                @endif
 
-            <div class="card mt-2">
-                <ul class="list-group">
-                    <a href="#" class="list-group-item list-group-item-action active">Miembros</a>
-                    @if(!$members)
-                        <li class="list-group-item d-flex justify-content-between align-items-center">
-                            <i class="fa-solid fa-user-slash"></i>  No hay miembros registrados.
-                        </li>
-                    @endif
-                    @foreach($members as $m)
-                        <li class="list-group-item d-flex justify-content-between align-items-center">
-                            {{$m->name}}
-                            @if($m->payment_status['paid'])
-                                <span class="badge bg-success rounded-pill"> Pagado</span>
-                            @elseif($m->payment_status['a_piece'])
-                                <span class="badge bg-success rounded-pill"> Abonado</span>
-                            @else
-                                <span class="badge bg-success rounded-pill"> Pendiente</span>
-                            @endif
-
-                        </li>
-                    @endforeach
-                </ul>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
